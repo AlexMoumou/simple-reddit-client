9181c4cf33e7cb75167ad0e48f512152d845fbe9
alex_moumoulidis <alexmoumoulides@gmail.com>
2023-02-19 19:03:51 +0200

    test: adds a couple of ui tests

--- SimpleRedditClientUITests/SimpleRedditClientUITests.swift
+++ SimpleRedditClientUITests/SimpleRedditClientUITests.swift
@@ -10,7 +10,7 @@ import XCTest
 class SimpleRedditClientUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        XCUIDevice.shared.orientation = .portrait
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
@@ -22,20 +22,42 @@ class SimpleRedditClientUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testOpenCloseSearch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert(app.staticTexts["Best Reddit Posts"].exists)
+        app.navigationBars["Best Reddit Posts"].buttons["Search"].tap()
+        XCTAssert(!app.staticTexts["Best Reddit Posts"].exists)
+        app.navigationBars["_TtGC7SwiftUI19UIHosting"]/*@START_MENU_TOKEN@*/.buttons["Cancel"]/*[[".otherElements[\"Cancel\"].buttons[\"Cancel\"]",".buttons[\"Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.staticTexts["Best Reddit Posts"].exists)
     }
+    
+    func testSearchSubredditByTextOpenAndThenClose() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.navigationBars["Best Reddit Posts"].buttons["Search"].tap()
+        
+        let textfield = app.navigationBars["_TtGC7SwiftUI19UIHosting"].textFields["Search"]
+        XCTAssert(textfield.exists)
+        textfield.tap()
+        textfield.typeText("FLOOF")
+        
+        let description = app.staticTexts["Floofy puffy cats and dogs and birds and chinchillas and cows and bunnies and caterpillars and anything else!"]
+        let exists = NSPredicate(format: "exists == 1")
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        expectation(for: exists, evaluatedWith: description, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        app.scrollViews.otherElements.staticTexts["r/Floof"].tap()
+        
+        XCTAssert(!textfield.exists)
+        XCTAssert(app.staticTexts["r/Floof"].exists)
+        
+        app.navigationBars["r/Floof"].buttons["Back"].tap()
+        
+        XCTAssert(textfield.exists)
     }
 }
